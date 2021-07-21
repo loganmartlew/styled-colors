0a6a37d01b2d37c85289d91594cc3153e96cd195
Logan Martlew <logan.martlew@gmail.com>
2021-07-21 23:16:39 +1200

    Added test suite for createHslaString

---
+++ src/util/createHslaString.test.ts
@@ -0,0 +1,100 @@
+import createHslaString from './createHslaString';
+import { HSLA } from '../types';
+
+const colorDefault: HSLA = {
+  hue: 0,
+  saturation: 0,
+  lightness: 0,
+  alpha: 1,
+};
+
+const colorDefaultHsla = 'hsla(0, 0%, 0%, 1)';
+
+test(`convert colorDefault to string, should be '${colorDefaultHsla}'`, () => {
+  expect(createHslaString(colorDefault)).toBe(colorDefaultHsla);
+});
+
+const colorRed: HSLA = {
+  hue: 0,
+  saturation: 100,
+  lightness: 50,
+  alpha: 1,
+};
+
+const colorRedHsla = 'hsla(0, 100%, 50%, 1)';
+
+test(`convert colorRed to string, should be '${colorRedHsla}'`, () => {
+  expect(createHslaString(colorRed)).toBe(colorRedHsla);
+});
+
+const colorGreen: HSLA = {
+  hue: 120,
+  saturation: 100,
+  lightness: 50,
+  alpha: 1,
+};
+
+const colorGreenHsla = 'hsla(120, 100%, 50%, 1)';
+
+test(`convert colorGreen to string, should be '${colorGreenHsla}'`, () => {
+  expect(createHslaString(colorGreen)).toBe(colorGreenHsla);
+});
+
+const colorBlue: HSLA = {
+  hue: 240,
+  saturation: 100,
+  lightness: 50,
+  alpha: 1,
+};
+
+const colorBlueHsla = 'hsla(240, 100%, 50%, 1)';
+
+test(`convert colorBlue to string, should be '${colorBlueHsla}'`, () => {
+  expect(createHslaString(colorBlue)).toBe(colorBlueHsla);
+});
+
+const colorTransparent: HSLA = {
+  hue: 0,
+  saturation: 0,
+  lightness: 0,
+  alpha: 0,
+};
+
+const colorTransparentHsla = 'hsla(0, 0%, 0%, 0)';
+
+test(`convert colorTransparent to string, should be '${colorTransparentHsla}'`, () => {
+  expect(createHslaString(colorTransparent)).toBe(colorTransparentHsla);
+});
+
+const colorMissingSaturation = {
+  hue: 0,
+  lightness: 100,
+  alpha: 1,
+};
+
+test(`color with missing property should throw`, () => {
+  expect(() => createHslaString(colorMissingSaturation as HSLA)).toThrow(
+    'Saturation was not provided'
+  );
+});
+
+const colorLightnessOutOfRange = {
+  hue: 0,
+  saturation: 50,
+  lightness: 361,
+  alpha: 1,
+};
+
+test(`color with invalid property should throw`, () => {
+  expect(() => createHslaString(colorLightnessOutOfRange as HSLA)).toThrow(
+    'Lightness was not in range 0-100'
+  );
+});
+
+type CreateHslaStringAny = (hsla?: HSLA) => string;
+const createHslaStringAny: CreateHslaStringAny =
+  createHslaString as CreateHslaStringAny;
+
+test(`providing no arguments should throw`, () => {
+  expect(() => createHslaStringAny()).toThrow('HSLA color was not provided');
+});
--- src/util/createHslaString.ts
+++ src/util/createHslaString.ts
@@ -1,6 +1,22 @@
 import { HSLA } from '../types';
 
 const createHslaString = (hsla: HSLA) => {
+  if (!hsla) throw new Error('HSLA color was not provided');
+
+  if (hsla.hue == null) throw new Error('Hue was not provided');
+  if (hsla.saturation == null) throw new Error('Saturation was not provided');
+  if (hsla.lightness == null) throw new Error('Lightness was not provided');
+  if (hsla.alpha == null) throw new Error('Alpha was not provided');
+
+  if (hsla.hue < 0 || hsla.hue > 360)
+    throw new Error('Hue was not in range 0-100');
+  if (hsla.saturation < 0 || hsla.saturation > 100)
+    throw new Error('Saturation was not in range 0-100');
+  if (hsla.lightness < 0 || hsla.lightness > 100)
+    throw new Error('Lightness was not in range 0-100');
+  if (hsla.alpha < 0 || hsla.alpha > 1)
+    throw new Error('Alpha was not in range 0-1');
+
   const string = `hsla(${hsla.hue}, ${hsla.saturation}%, ${hsla.lightness}%, ${hsla.alpha})`;
   return string;
 };
